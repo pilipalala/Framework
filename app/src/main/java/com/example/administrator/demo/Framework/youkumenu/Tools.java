2cf0501fc0f2c2f0115541ae336719b76dd661af
apk_wangyujie <809688547@qq.com>
2017-02-10 18:30:52 +0800

    使用属性动画

--- app/src/main/java/com/example/administrator/demo/Framework/youkumenu/Tools.java
+++ app/src/main/java/com/example/administrator/demo/Framework/youkumenu/Tools.java
@@ -1,9 +1,7 @@
 package com.example.administrator.demo.Framework.youkumenu;
 
-import android.view.View;
+import android.animation.ObjectAnimator;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
 
 /**
  * Created by Administrator on 2017/1/16.
@@ -19,7 +17,8 @@ public class Tools {
     }
 
     public static void hideView(ViewGroup view, int time) {
-        RotateAnimation rotateAnimation = new RotateAnimation(0, 180, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 1f);
+        /*试图动画*/
+        /*RotateAnimation rotateAnimation = new RotateAnimation(0, 180, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 1f);
         rotateAnimation.setDuration(1000);//设置动画播放持续时间
         rotateAnimation.setStartOffset(time);//动画停留在播放完成的状态
         rotateAnimation.setFillAfter(true);//延迟time ms后播放动画
@@ -27,11 +26,22 @@ public class Tools {
         for (int i = 0; i < view.getChildCount(); i++) {
             View childen = view.getChildAt(i);
             childen.setEnabled(false);
-        }
+        }*/
+
+        /*属性动画*/
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view, "rotation", 0, 180);
+        animator.setDuration(1000);
+        animator.setStartDelay(time);
+        animator.start();
+
+        view.setPivotX(view.getWidth() / 2);
+        view.setPivotY(view.getHeight());
+
     }
 
     public static void showView(ViewGroup view, int time) {
-        RotateAnimation rotateAnimation = new RotateAnimation(180, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 1f);
+        /*视图动画*/
+        /*RotateAnimation rotateAnimation = new RotateAnimation(180, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 1f);
         rotateAnimation.setDuration(1000);
         rotateAnimation.setStartOffset(time);//动画停留在播放完成的状态
         rotateAnimation.setFillAfter(true);
@@ -39,6 +49,14 @@ public class Tools {
         for (int i = 0; i < view.getChildCount(); i++) {
             View childen = view.getChildAt(i);
             childen.setEnabled(true);
-        }
+        }*/
+        /*属性动画*/
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view, "rotation", 180, 360);
+        animator.setDuration(1000);
+        animator.setStartDelay(time);
+        animator.start();
+
+        view.setPivotX(view.getWidth() / 2);
+        view.setPivotY(view.getHeight());
     }
 }
