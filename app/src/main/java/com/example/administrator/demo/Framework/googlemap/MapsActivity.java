c97fa1fff27f5645db18cbfe7cb5a5233fcb2a9d
pilipala <wangyujiew@sina.com>
2017-02-17 22:42:21 +0800

    添加谷歌地图

---
+++ app/src/main/java/com/example/administrator/demo/Framework/googlemap/MapsActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.demo.Framework.googlemap;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.administrator.demo.R;
+
+public class MapsActivity extends FragmentActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+    }
+
+
+}
